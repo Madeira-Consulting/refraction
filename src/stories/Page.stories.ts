98cb1e13bafbaa67eeb2290ab007f4d1749b1291
MarNeumann <mail@marvinneumann.com>
2023-02-11 19:07:50 +0100

    🛠 Storybook Page Wrapper

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,29 +1,55 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
+import type { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent } from "@storybook/testing-library";
 
-import { Page } from './Page';
+import { Page } from "./Page";
 
 const meta: Meta<typeof Page> = {
-  title: 'Example/Page',
-  component: Page,
-  parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+    title: "Example/Page",
+    component: Page,
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
+        layout: "fullscreen",
+    },
 };
 
 export default meta;
 type Story = StoryObj<typeof Page>;
 
-export const LoggedOut: Story = {};
-
-// More on interaction testing: https://storybook.js.org/docs/7.0/react/writing-tests/interaction-testing
-export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
-    });
-    await userEvent.click(loginButton);
-  },
+export const HomeScreen: Story = {
+    args: {
+        screenName: "home",
+        user: {
+            userName: "johndoe",
+            firstName: "John",
+            lastName: "Doe",
+            mail: "johndoe@mail.com",
+            profilePicture:
+                "https://i1.sndcdn.com/artworks-DoavOeHRnAqXbfO7-6FJP0A-t500x500.jpg",
+        },
+    },
 };
+
+export const PlayerScreen: Story = {
+    args: {
+        screenName: "player",
+        user: {
+            userName: "johndoe",
+            firstName: "John",
+            lastName: "Doe",
+            mail: "johndoe@mail.com",
+            profilePicture:
+                "https://i1.sndcdn.com/artworks-DoavOeHRnAqXbfO7-6FJP0A-t500x500.jpg",
+        },
+    },
+};
+
+// // More on interaction testing: https://storybook.js.org/docs/7.0/react/writing-tests/interaction-testing
+// export const LoggedIn: Story = {
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement);
+//     const loginButton = await canvas.getByRole('button', {
+//       name: /Log in/i,
+//     });
+//     await userEvent.click(loginButton);
+//   },
+// };
--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -4,6 +4,10 @@ import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 import "./page.css";
 import { FaAngleDown } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { ListItem } from "./ListItem";
+import { Home } from "./Home";
+import { Player } from "./Player";
 
 type User = {
     userName: string;
@@ -13,66 +17,59 @@ type User = {
     mail: string;
 };
 
-export const Page: React.FC = () => {
-    const [user, setUser] = React.useState<User>({
-        userName: "test",
-        firstName: "Test",
-        lastName: "User",
-        profilePicture:
-            "https://i1.sndcdn.com/artworks-DoavOeHRnAqXbfO7-6FJP0A-t500x500.jpg",
-        mail: "test",
-    });
+interface PageProps {
+    user?: User;
+    screenName?: string;
+    collapsed?: boolean;
+}
 
-    return (
-        <>
-            <div className="flex flex-row w-screen dark:bg-dark1">
-                <div className="fixed">
-                    <Sidebar
-                        user={user}
-                        isCollapsed={true}
-                        onLogin={function (): void {
-                            throw new Error("Function not implemented.");
-                        }}
-                        onLogout={function (): void {
-                            throw new Error("Function not implemented.");
-                        }}
-                        onProfile={function (): void {
-                            throw new Error("Function not implemented.");
-                        }}
-                    />
-                </div>
+export const Page = ({ user, screenName, collapsed }: PageProps) => (
+    <>
+        <div className="flex flex-row w-screen dark:bg-hero-lg bg-cover">
+            <div className="fixed">
+                <Sidebar
+                    user={user}
+                    isCollapsed={collapsed}
+                    onLogin={function (): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                    onLogout={function (): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                    onProfile={function (): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                />
+            </div>
 
-                <div className="flex flex-col w-full ml-24 px-10">
-                    <Header
-                        user={user}
-                        onLogin={() =>
-                            setUser({
-                                userName: "test",
-                                firstName: "Test",
-                                lastName: "User",
-                                profilePicture:
-                                    "https://i1.sndcdn.com/artworks-DoavOeHRnAqXbfO7-6FJP0A-t500x500.jpg",
-                                mail: "test",
-                            })
-                        }
-                        onLogout={() => setUser(undefined)}
-                        onProfile={() => setUser(undefined)}
-                    />
-                    <div className="grid grid-cols-3 gap-4 p-5">
-                        <div className="text-black col-span-2">Test</div>
-                        <div className="text-black">
-                            <div className="flex flex-col gap-8">
-                                <div className="bg-gray-100 dark:bg-dark2 p-5 font-bold text-slate-500 dark:text-white rounded-2xl flex flex-row items-center justify-between">
-                                    Match League Of Legends
-                                    <FaAngleDown />
-                                </div>
-                                <div className="bg-gray-100 dark:bg-dark2 h-96 p-5 font-bold text-slate-500 dark:text-white rounded-2xl flex flex-row items-center justify-between"></div>
-                                <div className="bg-gray-100 dark:bg-dark2 h-96 p-5 font-bold text-slate-500 dark:text-white rounded-2xl flex flex-row items-center justify-between"></div>
-                            </div>
-                        </div>
-                    </div>
+            <div
+                className={
+                    "fixed z-50 w-full " + (collapsed ? "pl-24" : "pl-72")
+                }
+            >
+                <Header
+                    user={user}
+                    onLogin={function (): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                    onLogout={function (): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                    onProfile={function (): void {
+                        throw new Error("Function not implemented.");
+                    }}
+                />
+            </div>
+            <div
+                className={
+                    "w-full h-full pt-24 bg-gradient-to-r from-dark1 via-dark1/98 to-dark1/95 "
+                }
+            >
+                <div className={"pr-8 " + (collapsed ? "pl-32" : "pl-72")}>
+                    {screenName == "home" ? <Home /> : <></>}
+                    {screenName == "player" ? <Player /> : <></>}
                 </div>
             </div>
-        </>
-    );
-};
+        </div>
+    </>
+);
